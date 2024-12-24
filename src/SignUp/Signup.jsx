d0b51f74b945c61326882db893d2ba16111c6464
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-24 15:46:11 +0200

    craeted the file state

--- src/SignUp/Signup.jsx
+++ src/SignUp/Signup.jsx
@@ -1,46 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Signup.css";
 import {useForm} from "react-hook-form";
+
 const Signup = () => {
     const {register, handleSubmit} = useForm({
         defaultValues: {
             username: "",
             email: "",
             password: "",
-            file: "",
         },
     });
+    const [file, setFile] = useState();
     const onSubmit = (data) => {
         console.log(data);
     };
+
     return (
         <div className="center">
             <form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <p className="title">Register </p>
                 <p className="message">Signup now and get full access to our app. </p>
                 <label>
-                    <input {...register("username")} className="input" type="text" placeholder required />
+                    <input
+                        {...register("username", {required: true})}
+                        className="input"
+                        type="text"
+                        placeholder
+                        required
+                    />
                     <span>Firstname</span>
                 </label>
                 <label>
-                    <input {...register("email")} className="input" type="text" placeholder required />
+                    <input
+                        {...register("email", {required: true})}
+                        className="input"
+                        type="text"
+                        placeholder
+                        required
+                    />
                     <span>Email</span>
                 </label>
                 <label>
-                    <input {...register("password")} className="input" type="password" placeholder required />
+                    <input
+                        {...register("password", {required: true})}
+                        className="input"
+                        type="password"
+                        placeholder
+                        required
+                    />
                     <span>Password</span>
                 </label>
 
-                <button class="container-btn-file">
+                <button className="container-btn-file">
                     Upload File
-                    <input class="file" name="text" type="file" />
+                    <input className="file" name="text" type="file" />
                 </button>
 
                 <button className="submit" type="submit">
                     Submit
                 </button>
                 <p className="signin">
-                    Already have an acount ? <a href="#">Signin</a>
+                    Already have an account? <a href="#">Signin</a>
                 </p>
             </form>
         </div>
