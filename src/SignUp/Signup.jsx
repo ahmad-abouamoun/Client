979b4782e420df6db3ed6fb2d1b4cc11d4066327
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-24 15:37:26 +0200

    imported the useform hook and used it

--- src/SignUp/Signup.jsx
+++ src/SignUp/Signup.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Signup.css";
+import {useForm} from "react-hook-form";
 const Signup = () => {
+    const {register, handleSubmit} = useForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+            file: "",
+        },
+    });
+
     return (
         <div className="center">
             <form className="form">
@@ -24,7 +34,9 @@ const Signup = () => {
                     <input class="file" name="text" type="file" />
                 </button>
 
-                <button className="submit">Submit</button>
+                <button className="submit" type="submit">
+                    Submit
+                </button>
                 <p className="signin">
                     Already have an acount ? <a href="#">Signin</a>
                 </p>
