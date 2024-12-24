fb2f08d4b1cae787543d9a817db7274875555736
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-24 14:45:23 +0200

    styled the title of the signup

--- src/SignUp/Signup.jsx
+++ src/SignUp/Signup.jsx
@@ -3,7 +3,10 @@ import "./Signup.css";
 const Signup = () => {
     return (
         <div className="center">
-            <form className="form"></form>
+            <form className="form">
+                <p className="title">Register </p>
+                <p className="message">Signup now and get full access to our app. </p>
+            </form>
         </div>
     );
 };
