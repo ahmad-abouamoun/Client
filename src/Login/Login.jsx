02c5f46c33e5be434598178ff9fd5d0975caf0b3
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 14:11:51 +0200

    added the necessary elements for the login form

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -12,7 +12,38 @@ const Login = () => {
 
     return (
         <div className="center">
-            <form className="form" onSubmit={handleSubmit(onSubmit)}></form>
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                <p className="title">Login</p>
+                <p className="message">Login now and get full access to our app.</p>
+
+                <label>
+                    <input
+                        {...register("email", {required: true})}
+                        className="input"
+                        type="text"
+                        placeholder
+                        required
+                    />
+                    <span>Email</span>
+                </label>
+                <label>
+                    <input
+                        {...register("password", {required: true})}
+                        className="input"
+                        type="password"
+                        placeholder
+                        required
+                    />
+                    <span>Password</span>
+                </label>
+
+                <button className="submit" type="submit">
+                    Submit
+                </button>
+                <p className="signin">
+                    Do not have an account? <a href="#">Signup</a>
+                </p>
+            </form>
         </div>
     );
 };
