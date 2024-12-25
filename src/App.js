52df0003fd62c2e28d13ab4bf1531f10d9d23444
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 14:22:27 +0200

    started working on the HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {Route, Router} from "react-router";
 import Signup from "./SignUp/Signup";
 import Login from "./Login/Login";
+import HomePage from "./HomePage/HomePage";
 
 function App() {
     return (
         <div>
-            <Login />
+            <HomePage />
         </div>
     );
 }
