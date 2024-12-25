a8d7cec9379a4a4416bc02f7c42dcb9aff8fcef1
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 14:24:21 +0200

    created the navbar

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,4 +1,24 @@
 const HomePage = () => {
-    return <div>Hello World</div>;
+    return (
+        <div>
+            <nav>
+                <h3>Website Name</h3>
+
+                <ul>
+                    <li>
+                        <span>Home</span>
+                    </li>
+                    <li>
+                        <span>About Us</span>
+                    </li>
+                    <li>
+                        <span>Profile</span>
+                    </li>
+                </ul>
+
+                <button>Login</button>
+            </nav>
+        </div>
+    );
 };
 export default HomePage;
