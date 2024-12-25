7bb1446b334528256daf849b488d328d60ce026e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 15:45:46 +0200

    manipulated the opacity of the background image

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,10 +1,16 @@
+import {useState} from "react";
 import NavBar from "../Re-usableComponents/NavBar/NavBar";
+import image1 from "../Assets/dietBackground.jpg";
+import image2 from "../Assets/gymBackground.webp";
+
 import "./HomePage.css";
 const HomePage = () => {
+    const [image, setImage] = useState("");
+    console.log(image1);
     return (
         <div>
             <div>
-                <div className="backGround">
+                <div className="backGround" style={{backgroundImage: `url(${image2})`}}>
                     <NavBar />
                     <div className="blackBox">
                         <h1>Welcome to Balance Beacon</h1>
