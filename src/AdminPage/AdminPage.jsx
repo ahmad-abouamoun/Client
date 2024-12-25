7ee1505a7e408292acda06710738893d6c54636d
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 18:17:26 +0200

    created the column variable

--- src/AdminPage/AdminPage.jsx
+++ src/AdminPage/AdminPage.jsx
@@ -1,4 +1,29 @@
+import React from "react";
+import DataTable from "react-data-table-component";
+
 const AdminPage = () => {
+    const columns = [
+        {
+            name: "Name",
+            selector: (row) => row.name,
+            sortable: true,
+        },
+        {
+            name: "Email",
+            selector: (row) => row.email,
+            sortable: true,
+        },
+        {
+            name: "Banned",
+            selector: (row) => (row.banned ? "Yes" : "No"),
+        },
+        {
+            name: "Actions",
+            cell: (row) => <button>Click Me</button>,
+        },
+    ];
+
     return <div></div>;
 };
+
 export default AdminPage;
