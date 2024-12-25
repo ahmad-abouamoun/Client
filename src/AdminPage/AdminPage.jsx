9cbfd84900e9fd80709b9d70ff809ff2592b0eee
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 18:17:47 +0200

    created fummy data to test the table

--- src/AdminPage/AdminPage.jsx
+++ src/AdminPage/AdminPage.jsx
@@ -23,7 +23,15 @@ const AdminPage = () => {
         },
     ];
 
-    return <div></div>;
+    const data = [
+        {id: 1, name: "John Doe", email: "john@example.com", banned: false},
+        {id: 2, name: "Jane Smith", email: "jane@example.com", banned: true},
+        {id: 3, name: "Alice Johnson", email: "alice@example.com", banned: false},
+    ];
+
+    return (
+       
+    );
 };
 
 export default AdminPage;
