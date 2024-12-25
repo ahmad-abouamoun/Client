646b9814f8eef6856b3b46cd4ecaef55565e6a7d
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 15:30:57 +0200

    made sure the css is working

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -3,12 +3,14 @@ import "./HomePage.css";
 const HomePage = () => {
     return (
         <div>
-            <div className="backGround">
-                <NavBar />
-                <div className="blackBox">
-                    <h1>Welcome to Balance Beacon</h1>
-                    <p>Book a meeting now with one of our specialists</p>
-                    <button>Book Now</button>
+            <div>
+                <div className="backGround">
+                    <NavBar />
+                    <div className="blackBox">
+                        <h1>Welcome to Balance Beacon</h1>
+                        <p>Book a meeting now with one of our specialists</p>
+                        <button>Book Now</button>
+                    </div>
                 </div>
             </div>
         </div>
