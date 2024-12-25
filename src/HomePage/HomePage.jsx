125a7ff6db41abb0a0dd858e08cc419a4a5605ba
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-25 15:18:48 +0200

    created the black box in the homepage

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
 import NavBar from "../Re-usableComponents/NavBar/NavBar";
-
+import "./HomePage.css";
 const HomePage = () => {
     return (
         <div>
-            <div>
+            <div className="backGround">
                 <NavBar />
+                <div className="blackBox">
+                    <h1>Welcome to Balance Beacon</h1>
+                    <p>Book a meeting now with one of our specialists</p>
+                    <button>Book Now</button>
+                </div>
             </div>
         </div>
     );
