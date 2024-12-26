3d0c49ccbd8cbcbc539004ab0ff6f9f2800a9a15
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-26 13:51:56 +0200

    used the navBar in the diet page

--- src/Diet/Diet.jsx
+++ src/Diet/Diet.jsx
@@ -1,4 +1,30 @@
+import image1 from "../Assets/foodBackground.jpg";
+import BlackBox from "../Re-usableComponents/BlackBox/BlackBox";
+import NavBar from "../Re-usableComponents/NavBar/NavBar";
+
 const DietPage = () => {
-    return <div>Hello world</div>;
+    return (
+        <div>
+            <div className="backGround" style={{backgroundImage: `url(${image1})`}}>
+                <NavBar>
+                    <ul>
+                        <li>
+                            <span>Trainning</span>
+                        </li>
+                        <li>
+                            <span>Diet</span>
+                        </li>
+                        <li>
+                            <span>Mental Health</span>
+                        </li>
+                        <li>
+                            <span>Meetings</span>
+                        </li>
+                    </ul>
+                    <img src={image1} alt="" />
+                </NavBar>
+            </div>
+        </div>
+    );
 };
 export default DietPage;
