cf78a442b35e20de5bb108423f245d6e8e15780e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-27 13:56:26 +0200

    started working on the ProfilePage

---
+++ src/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProfilePage.css";
+import image from "./Assets/dietBackground.jpg";
+const ProfilePage = () => {
+    return <div className="profile-container"></div>;
+};
+
+export default ProfilePage;
