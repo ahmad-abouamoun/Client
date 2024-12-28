dc559dfbcea16e03321aa959a7deb0ac598d5dbd
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-28 13:48:49 +0200

    craeted the state food that and started working using the useEffect hook

--- src/Diet/Diet.jsx
+++ src/Diet/Diet.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import image1 from "../Assets/foodBackground.jpg";
 import Card from "../Re-usableComponents/Card/Card";
 import NavBar from "../Re-usableComponents/NavBar/NavBar";
@@ -6,8 +6,10 @@ import BlackBox from "../Re-usableComponents/BlackBox/BlackBox";
 import "./Diet.css";
 
 const DietPage = () => {
+    const [foods, setFoods] = useState();
+    useEffect(() => {}, []);
     return (
-        <div style={{position: "relative"}}>
+        <div>
             <div className="backGround" style={{backgroundImage: ` url(${image1})`}}>
                 <NavBar>
                     <ul>
@@ -27,7 +29,6 @@ const DietPage = () => {
                     <img src={image1} alt="Diet Background" />
                 </NavBar>
 
-                {/* BlackBox Section */}
                 <BlackBox>
                     <h1>"Let food be thy medicine and medicine be thy food." - Hippocrates</h1>
                     <button>Book Now</button>
