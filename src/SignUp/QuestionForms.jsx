908284eb2e092c1d8afea70d8b5164f79e0dd42e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-29 14:42:34 +0200

    added the necessary stuff for the question forms component

--- src/SignUp/QuestionForms.jsx
+++ src/SignUp/QuestionForms.jsx
@@ -2,7 +2,23 @@ import "./QuestionForm.css";
 const QuestionFroms = () => {
     return (
         <div className="questionnaire-container">
-            <div className="overlay"></div>
+            <div className="overlay">
+                <div className="questionnaire-content">
+                    <h2>Just a couple of questions before creating your account</h2>
+                    <p>Do you have Diabetes?</p>
+                    <div className="options">
+                        <label>
+                            <input type="radio" name="diabetes" value="yes" />
+                            Yes
+                        </label>
+                        <label>
+                            <input type="radio" name="diabetes" value="no" />
+                            No
+                        </label>
+                    </div>
+                    <button className="next-button">Next</button>
+                </div>
+            </div>
         </div>
     );
 };
