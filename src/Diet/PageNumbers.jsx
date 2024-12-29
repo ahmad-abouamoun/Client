cfcfdcc17d1c8235944b4070e576dc9067941a24
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-29 13:27:29 +0200

    created the logic for the PageNumber component

--- src/Diet/PageNumbers.jsx
+++ src/Diet/PageNumbers.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "./PageNumber.css";
 
-const PageNumber = () => {
-    
+const PageNumber = ({numItems, setNumber}) => {
+    const radioButtons = [];
+
+    for (let i = 1; i <= numItems; i++) {
+        radioButtons.push(
+            <label key={i}>
+                <input
+                    type="radio"
+                    name="value-radio"
+                    id={`value-${i}`}
+                    value={`Value ${i}`}
+                    defaultChecked={i === 1}
+                    onChange={() => setNumber(i)}
+                />
+                <span>{i}</span>
+            </label>
+        );
+    }
 
     return (
-        
+        <div className="radio-input" style={{"--num_items": numItems}}>
+            {radioButtons}
+            <span className="selection"></span>
+        </div>
     );
 };
 
