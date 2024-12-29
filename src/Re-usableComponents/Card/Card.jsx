bc601b3edba51ad2ade8c9ee70ad00adba9d1ac7
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-29 13:25:02 +0200

    created the file component structure

--- src/Re-usableComponents/Card/Card.jsx
+++ src/Re-usableComponents/Card/Card.jsx
@@ -2,7 +2,31 @@ import "./Card.css";
 import image1 from "../../Assets/dietBackground.jpg";
 import React, {useState} from "react";
 
-function Card() {
-    return <div></div>;
+function Card({title, description}) {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleLearnMore = () => {
+        setShowPopup(true);
+    };
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+    return (
+        <div>
+            <div className={`card`}>
+                <img className="card-image" src={image1} alt="Healthy Eating" />
+                <div className="card-content">
+                    <h2 className="card-title">{title}</h2>
+                    <p className="card-description">{description}</p>
+                </div>
+                <div className="card-actions">
+                    <button className="learn-more" onClick={handleLearnMore}>
+                        Learn More
+                    </button>
+                    <button className="bookmark">Bookmark</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Card;
