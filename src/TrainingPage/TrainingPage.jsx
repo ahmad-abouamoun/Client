e407e7c3a216730d4589cd9c2f98e5455b8e90d5
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-12-30 17:03:57 +0200

    added the navbar to the training page

--- src/TrainingPage/TrainingPage.jsx
+++ src/TrainingPage/TrainingPage.jsx
@@ -1,4 +1,28 @@
+import NavBar from "../Re-usableComponents/NavBar/NavBar";
+import image from "../Assets/training.jpg";
+import image1 from "../Assets/foodBackground.jpg";
+
 const TrainingPage = () => {
-    return <div>Hello World</div>;
+    return (
+        <div>
+            <NavBar>
+                <ul>
+                    <li>
+                        <span>Training</span>
+                    </li>
+                    <li>
+                        <span>Diet</span>
+                    </li>
+                    <li>
+                        <span>Mental Health</span>
+                    </li>
+                    <li>
+                        <span>Meetings</span>
+                    </li>
+                </ul>
+                <img src={image1} alt="Diet Background" />
+            </NavBar>
+        </div>
+    );
 };
 export default TrainingPage;
