a0ac07b4d49f8eea86a20a80b269b9db9a11ffc4
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-02 13:57:35 +0200

    created the logic to show and close the modal to book a meeting and select a date

--- src/BookingPage/BookingPage.jsx
+++ src/BookingPage/BookingPage.jsx
@@ -14,7 +14,7 @@ const localizer = dateFnsLocalizer({
 });
 
 const BookingPage = () => {
-    const [events, setEvents] = useState([]);
+    const [events, setEvents] = useState();
     const [showModal, setShowModal] = useState(false);
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [newEvent, setNewEvent] = useState({
@@ -25,6 +25,20 @@ const BookingPage = () => {
         room: "",
     });
 
+    const Experts = {
+        nutritionist: "Room 101",
+        therapist: "Room 102",
+        coach: "Room 103",
+    };
+
+    const meetingOptions = Object.keys(Experts);
+
+    const handleSlotSelect = (slotInfo) => {
+        setSelectedDate(slotInfo.start);
+        setNewEvent({title: "Meeting", start: "", end: "", type: "", room: ""});
+        setShowModal(true);
+    };
+
     return (
         <div>
             <Calendar
@@ -34,10 +48,30 @@ const BookingPage = () => {
                 endAccessor="end"
                 style={{height: 500}}
                 selectable
+                onSelectSlot={handleSlotSelect}
                 min={new Date(2024, 0, 1, 8, 0)}
                 max={new Date(2024, 0, 1, 18, 0)}
                 step={30}
             />
+
+            {showModal && (
+                <div className="modal">
+                    <h2 className="modal-header">Add Event</h2>
+                    <p className="modal-text">Selected Date: {format(selectedDate, "MMMM d, yyyy")}</p>
+
+                    <br />
+
+                    <p className="modal-text">
+                        Assigned Room: <strong>{newEvent.room || "No room assigned yet"}</strong>
+                    </p>
+                    <br />
+
+                    <button className="modal-button add">Add Meeting</button>
+                    <button className="modal-button cancel" onClick={() => setShowModal(false)}>
+                        Cancel
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
