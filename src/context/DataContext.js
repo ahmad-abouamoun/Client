fd0e204ce2750484d372b971b8fc8b7244ed0839
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-05 13:04:06 +0200

    created the logic for the loggedIn context

---
+++ src/context/DataContext.js
@@ -0,0 +1,11 @@
+import {useContext} from "react";
+import {useState} from "react";
+import {createContext} from "react";
+
+const DataContext = createContext();
+export const DataProvider = ({children}) => {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    return <DataContext.Provider value={{loggedIn, setLoggedIn}}>{children}</DataContext.Provider>;
+};
+export const useQuiz = () => useContext(DataContext);
--- src/context/DataProvider.js
+++
@@ -1,4 +0,0 @@
-const QuizContext = createContext();
-export const QuizProvider = () => {
-    return <div></div>;
-};
