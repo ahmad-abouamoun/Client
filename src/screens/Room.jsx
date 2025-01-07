ccf3285d4527228f729ffd4704f686996749023c
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-07 13:45:17 +0200

    created the necessary JSX for the Room page along with the statees i will need

--- src/screens/Room.jsx
+++ src/screens/Room.jsx
@@ -4,7 +4,31 @@ import peer from "../service/peer";
 import {useSocket} from "../context/SocketProvider";
 
 const Room = () => {
-    return <div></div>;
+    const socket = useSocket();
+    const [remoteSocketId, setRemoteSocketId] = useState(null);
+    const [myStream, setMyStream] = useState();
+    const [remoteStream, setRemoteStream] = useState();
+
+    return (
+        <div>
+            <h1>Room Page</h1>
+            <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
+            {myStream && <button>Send Stream</button>}
+            {remoteSocketId && <button>CALL</button>}
+            {myStream && (
+                <>
+                    <h1>My Stream</h1>
+                    <ReactPlayer playing height="500px" width="600px" url={myStream} />
+                </>
+            )}
+            {remoteStream && (
+                <>
+                    <h1>Remote Stream</h1>
+                    <ReactPlayer playing height="500px" width="600px" url={remoteStream} />
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Room;
