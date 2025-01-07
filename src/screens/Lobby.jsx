da23c80546e22cd386e3bde7d44a66bb7e2facdc
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-07 14:14:41 +0200

    fixed the design of the lobby a bit

--- src/screens/Lobby.jsx
+++ src/screens/Lobby.jsx
@@ -6,7 +6,7 @@ import "./Lobby.css";
 const Lobby = () => {
     const user = useSelector((state) => state.users.user);
     const email = user.email;
-    const [room, setRoom] = useState("");
+    const [room, setRoom] = useState("101");
 
     const socket = useSocket();
     const navigate = useNavigate();
@@ -39,8 +39,12 @@ const Lobby = () => {
             <div class="lobby">
                 <h1>Lobby</h1>
                 <form onSubmit={handleSubmitForm}>
-                    <label htmlFor="room">Room Number</label>
-                    <input type="text" id="room" value={room} onChange={(e) => setRoom(e.target.value)} />
+                    <label htmlFor="room">Expert Meeting With</label>
+                    <select name="room" id="room" onChange={(e) => setRoom(e.target.value)}>
+                        <option value="101">coach</option>
+                        <option value="102">therapist</option>
+                        <option value="103">nutritionist</option>
+                    </select>
                     <br />
                     <button>Join</button>
                 </form>
