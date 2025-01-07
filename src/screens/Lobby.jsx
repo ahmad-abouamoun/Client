1bf40e19de499e9700875fc39600a51524c5cd1e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-07 13:35:50 +0200

    started working on the Lobby Page jsx and state

--- src/screens/Lobby.jsx
+++ src/screens/Lobby.jsx
@@ -1,12 +1,25 @@
 import React, {useState, useCallback, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import {useSocket} from "../context/SocketProvider";
+import {useSelector} from "react-redux";
 
 const Lobby = () => {
-    
+    const user = useSelector((state) => state.users.user);
+    const email = user.email;
+    const [room, setRoom] = useState("");
+
+    const socket = useSocket();
+    const navigate = useNavigate();
+
     return (
         <div>
-            
+            <h1>Lobby</h1>
+            <form>
+                <label htmlFor="room">Room Number</label>
+                <input type="text" id="room" value={room} onChange={(e) => setRoom(e.target.value)} />
+                <br />
+                <button>Join</button>
+            </form>
         </div>
     );
 };
