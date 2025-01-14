9422c6f6d1e7a0287fc23e467a852d44e0e0e655
Ahmad Abou Amoun <aabouamoun@gmail.com>
2025-01-14 15:08:48 +0200

    finished the html for the footer component

--- src/Re-usableComponents/Footer/Footer.jsx
+++ src/Re-usableComponents/Footer/Footer.jsx
@@ -1,6 +1,51 @@
 import {Facebook, Instagram, Linkedin} from "lucide-react";
 import "./Footer.css";
 const Footer = () => {
-    return <div></div>;
+    return (
+        <div>
+            <footer>
+                <div class="footer-container">
+                    <div class="footer-section">
+                        <h3>Customer Service</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Lorem Ipsum</a>
+                            </li>
+                            <li>
+                                <a href="#">Lorem Ipsum</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-section">
+                        <h3>Information</h3>
+                        <ul>
+                            <li>
+                                <a href="#">My Account</a>
+                            </li>
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Affiliate Program</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-section follow-us">
+                        <h3>Follow us for latest updates</h3>
+                        <div>
+                            <Linkedin size={32} className="linkedin-icon" />
+                            <Instagram size={32} className="instagram-icon" />
+                            <Facebook size={32} className="facebook-icon" />
+                        </div>
+                        <div class="social-icons"></div>
+                        <p>Follow US</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
 };
 export default Footer;
